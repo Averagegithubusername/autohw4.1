8fcc5df16e2e1553a9cfc298d1f1436928c986a8
Averagegithubusername <s0mething_different@mail.ru>
2022-04-05 17:41:32 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/c16no39lqim4ntu2?svg=true)](https://ci.appveyor.com/project/Averagegithubusername/autohw4-1)
---
+++ src/test/java/CardWithDeliveryFormTest.java
@@ -0,0 +1,28 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class CardWithDeliveryFormTest {
+
+    @Test
+    void shouldSendForm() {
+        Configuration.holdBrowserOpen = true;
+        LocalDate date = LocalDate.now();
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Южно-Сахалинск");
+        $("[data-test-id='date'] input").clear();
+        $("[data-test-id='date'] input").setValue(date.plusDays(5).format(DateTimeFormatter.ofPattern("dd MM yyyy")));
+        $("[data-test-id='name'] input").setValue("Жан-Поль Бельмондо");
+        $("[data-test-id='phone'] input").setValue("12345678910");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(Condition.exactText("Запланировать")).click();
+        $("[data-test-id='replan-notification'] button").click();
+        $("[data-test-id='success-notification']").shouldBe(Condition.visible, Duration.ofSeconds(15));
+    }
+}
