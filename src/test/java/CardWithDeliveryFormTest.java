f690f3dd9ab08502c903db74ad024da4b7f1195a
Averagegithubusername <s0mething_different@mail.ru>
2022-04-08 12:09:55 +0300

    code optimisation

--- src/test/java/CardWithDeliveryFormTest.java
+++ src/test/java/CardWithDeliveryFormTest.java
@@ -15,17 +15,20 @@ public class CardWithDeliveryFormTest {
     void shouldSendForm() {
         Configuration.headless = true;
         Configuration.holdBrowserOpen = true;
-        LocalDate date = LocalDate.now();
+//        LocalDate date = LocalDate.now();
+        String date = LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Южно-Сахалинск");
         $("[data-test-id='date'] input").doubleClick();
         $("[data-test-id='date'] input").sendKeys(Keys.DELETE);
-        $("[data-test-id='date'] input").setValue(date.plusDays(5).format(DateTimeFormatter.ofPattern("dd MM yyyy")));
+//        $("[data-test-id='date'] input").setValue(date.plusDays(5).format(DateTimeFormatter.ofPattern("dd MM yyyy")));
+        $("[data-test-id='date'] input").setValue(date);
         $("[data-test-id='name'] input").setValue("Жан-Поль Бельмондо");
         $("[data-test-id='phone'] input").setValue("+12345678910");
         $("[data-test-id=agreement]").click();
         $$("button").find(Condition.exactText("Забронировать")).click();
         $("[data-test-id='notification']").shouldBe(Condition.visible, Duration.ofSeconds(15));
-        $(".notification__content").shouldHave(Condition.ownText(date.plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))));
+//        $(".notification__content").shouldHave(Condition.ownText(date.plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))));
+        $(".notification__content").shouldHave(Condition.ownText(date));
     }
 }
